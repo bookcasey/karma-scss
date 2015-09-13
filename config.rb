7cb00f856c4fea3c652cfa8016756688165252e9
Casey Olson <casey@bookcasey.com>
2015-09-12 20:40:29 -0500

    add partials, start compressing

--- config.rb
+++ config.rb
@@ -5,7 +5,7 @@ require 'compass/import-once/activate'
 http_path = "/"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style =  :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
